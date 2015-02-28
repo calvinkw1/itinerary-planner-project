1b7d47f0fde34e217791c57a4441f8581ffec10d
Calvin Wang <calvinkw1@gmail.com>
2015-02-27 16:53:58 -0800

    routes configured and controllers added

--- itineraries_lab/config/routes.rb
+++ itineraries_lab/config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
+  
+  root 'access#index'
+  resources :users
+  resources :itineraries
+  resources :destinations
+  
+
+#           Prefix Verb   URI Pattern                      Controller#Action
+#             root GET    /                                access#index
+#            users GET    /users(.:format)                 users#index
+#                  POST   /users(.:format)                 users#create
+#         new_user GET    /users/new(.:format)             users#new
+#        edit_user GET    /users/:id/edit(.:format)        users#edit
+#             user GET    /users/:id(.:format)             users#show
+#                  PATCH  /users/:id(.:format)             users#update
+#                  PUT    /users/:id(.:format)             users#update
+#                  DELETE /users/:id(.:format)             users#destroy
+#      itineraries GET    /itineraries(.:format)           itineraries#index
+#                  POST   /itineraries(.:format)           itineraries#create
+#    new_itinerary GET    /itineraries/new(.:format)       itineraries#new
+#   edit_itinerary GET    /itineraries/:id/edit(.:format)  itineraries#edit
+#        itinerary GET    /itineraries/:id(.:format)       itineraries#show
+#                  PATCH  /itineraries/:id(.:format)       itineraries#update
+#                  PUT    /itineraries/:id(.:format)       itineraries#update
+#                  DELETE /itineraries/:id(.:format)       itineraries#destroy
+#     destinations GET    /destinations(.:format)          destinations#index
+#                  POST   /destinations(.:format)          destinations#create
+#  new_destination GET    /destinations/new(.:format)      destinations#new
+# edit_destination GET    /destinations/:id/edit(.:format) destinations#edit
+#      destination GET    /destinations/:id(.:format)      destinations#show
+#                  PATCH  /destinations/:id(.:format)      destinations#update
+#                  PUT    /destinations/:id(.:format)      destinations#update
+#                  DELETE /destinations/:id(.:format)      destinations#destroy
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
