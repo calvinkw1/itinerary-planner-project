ce926406b7ae63fb4c005480586605e479bf2ae2
Brent Dady <brentdady@gmail.com>
2015-03-01 11:54:41 -0800

    comments working across models

--- itineraries_lab/app/controllers/destinations_controller.rb
+++ itineraries_lab/app/controllers/destinations_controller.rb
@@ -1,25 +1,17 @@
 class DestinationsController < ApplicationController
-<<<<<<< Updated upstream
   before_action :find_itinerary, only: [:new, :create, :add_destination]
   before_action :find_destination, only: [:creat, :show, :edit, :update, :destroy]
-  def index
-    @destinations = Destination.all
-  end
-  def new
-    
-=======
+
   def index
     @destination = Destination.all
   end
+  
   def new
->>>>>>> Stashed changes
     @destination = Destination.new
   end
 
   def create 
-<<<<<<< Updated upstream
     itinerary = Itinerary.find params[:id]
-
     @destination = Destination.create destination_params
     if @destination.save
       itinerary.destinations << @destination
@@ -35,15 +27,11 @@ class DestinationsController < ApplicationController
     # @itinerary = Itinerary.find params[:id]
     @destination = Destination.create(destination_params)
     if @destination.save
-      
       redirect_to destination_path
     else
       render :new
     end
-
-=======
     Destination.create 
->>>>>>> Stashed changes
   end
 
   def show
@@ -56,7 +44,6 @@ class DestinationsController < ApplicationController
   def edit
 
   end
-<<<<<<< Updated upstream
 
   def update 
     @destination.update_attributes destination_params
@@ -77,11 +64,10 @@ class DestinationsController < ApplicationController
    def find_itinerary
      @itinerary = Itinerary.find params[:id]
    end
-=======
+
   private
   # def destination_params
   #   params.require(:destination).permit(:name, :location, :start_date, :end_date)
   # end
->>>>>>> Stashed changes
   
 end
